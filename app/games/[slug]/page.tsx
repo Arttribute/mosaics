3684c651001498e3d80f0fd746b3f2af2a0e93c3
bashybaranaba <bashybaranaba@gmail.com>
2024-05-25 11:07:43 +0300

    feat: add puzzle timer

--- app/games/[slug]/page.tsx
+++ app/games/[slug]/page.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Image from "next/image";
 import TileSelector from "@/components/game/tile-selector";
+import PuzzleTimer from "@/components/game/puzzle-timer";
 
 export default function Game() {
   return (
@@ -8,7 +10,19 @@ export default function Game() {
         <div className="px-4 py-2 mx-8 mb-4 bg-amber-50 rounded-xl text-center max-w-96">
           <h1 className="text-lg font-bold">Brown fox jumps over lazy dog</h1>
         </div>
-        <TileSelector src="/puzzle1.jpeg" numCols={3} numRows={3} />
+
+        <PuzzleTimer
+          initialSeconds={60}
+          secondsLeft={60}
+          setSecondsLeft={() => {}}
+          isActive={false}
+          setIsActive={() => {}}
+        />
+        <TileSelector
+          src="https://res.cloudinary.com/arttribute/image/upload/v1715629309/vfgzrvx9zjnhuuabg4ix.jpg"
+          numCols={3}
+          numRows={3}
+        />
       </div>
     </div>
   );
