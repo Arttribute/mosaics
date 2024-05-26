57fec3bf5844605d2e421b93011271cdb3fc85ca
Nathan Nderitu <natenderitu@gmail.com>
2024-05-27 02:41:07 +0300

    feat: update hardhat config file with galadriel network information

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+
+const galadrielDevnet = ["19daf05aeaceeac68736f22cffeb780f59a892c94eba0a5227f465429b086428"]
+
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    galadriel: {
+    chainId: 696969,
+    url: "https://devnet.galadriel.com/",
+    accounts: galadrielDevnet,
+  },
+},
 };
