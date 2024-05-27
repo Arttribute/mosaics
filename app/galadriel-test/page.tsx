8ce7b3d6d9bde3ff5bb9d73d917a21f87ce76b66
Nathan Nderitu <natenderitu@gmail.com>
2024-05-27 04:46:02 +0300

    feat: create galadriel testing start chat page

---
+++ app/galadriel-test/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreateChat from '@/components/galadriel-test/initiate-chat'
+
+const page = () => {
+  return (
+    <div>
+      <CreateChat/>
+    </div>
+  )
+}
+
+export default page
