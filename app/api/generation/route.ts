15312efcb18861b5752bd146dd1c3fc1d1b5a6c5
bashybaranaba <bashybaranaba@gmail.com>
2024-05-29 00:16:57 +0300

    feat: add image generation api

---
+++ app/api/generation/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+
+const API_KEY = process.env.ASTRIA_API_KEY;
+
+export async function POST(request: Request) {
+  const { textToImageObject, modelId } = await request.json();
+
+  try {
+    const generationRes = await fetch(
+      `https://api.astria.ai/tunes/${modelId}/prompts`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + API_KEY,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(textToImageObject),
+      }
+    );
+
+    const generationData = await generationRes.json();
+
+    return new NextResponse(JSON.stringify(generationData), {
+      status: 201,
+    });
+  } catch (error: any) {
+    return new NextResponse(error.message, {
+      status: 500,
+    });
+  }
+}
