b2900881f3fc032334a8d91e4aca13cb785d0595
bashybaranaba <bashybaranaba@gmail.com>
2024-06-01 14:08:59 +0300

    refactor: remove cluter from deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,24 +2,21 @@ const hre = require("hardhat");
 require("dotenv").config();
 
 async function main() {
-  if (!process.env.ORACLE_ADDRESS) {
+  if (!process.env.GALADRIEL_ORACLE_ADDRESS) {
     throw new Error("ORACLE_ADDRESS env variable is not set.");
   }
-  const oracleAddress: string = process.env.ORACLE_ADDRESS;
-  await deployTest(oracleAddress);
+  const oracleAddress: string = process.env.GALADRIEL_ORACLE_ADDRESS;
+  await deployGameAgent(oracleAddress);
 }
 
-async function deployTest(oracleAddress: string) {
+async function deployGameAgent(oracleAddress: string) {
   const agent = await hre.ethers.deployContract(
     "MosaicsGameAgent",
     [oracleAddress],
     {}
   );
-
   await agent.waitForDeployment();
-  console.log(`MosaicsGameAgent oracle address ${process.env.ORACLE_ADDRESS}`);
   console.log(`MosaicsGameAgent contract deployed to ${agent.target}`);
-  console.log("MosaicsGameAgent contract address: ", await agent.getAddress());
 }
 
 main().catch((error) => {
