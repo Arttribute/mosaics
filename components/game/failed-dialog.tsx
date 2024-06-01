5b4286693f2cac9f836d15e0c2de022ad7c2d374
bashybaranaba <bashybaranaba@gmail.com>
2024-06-01 14:03:32 +0300

    feat: add fail puzzle diplay ui

---
+++ components/game/failed-dialog.tsx
@@ -0,0 +1,62 @@
+"use client";
+import * as React from "react";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import { ChevronRight } from "lucide-react";
+
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+
+export default function FailedDialog({
+  open,
+  solution,
+  timeRemaining,
+  movesRemaining,
+  onLeaveGame,
+}: {
+  open: boolean;
+  solution: string;
+  timeRemaining: number;
+  movesRemaining: number;
+  onLeaveGame: () => void;
+}) {
+  return (
+    <Dialog open={open}>
+      <DialogContent>
+        <div className="w-full">
+          <div className="p-2 flex flex-col items-center justify-center">
+            <Image
+              src={solution}
+              width={500}
+              height={500}
+              alt={"game"}
+              className="aspect-square rounded-md m-1"
+            />
+            {timeRemaining === 0 && (
+              <p className="text-base font-semibold  m-2 text-red-500">
+                Time is up!
+              </p>
+            )}
+            {movesRemaining === 0 && (
+              <p className="text-lg font-semibold  m-2 text-red-500">
+                No moves left!
+              </p>
+            )}
+
+            <p className="text-base font-semibold mb-2">
+              Failed to solve puzzle 😞{" "}
+            </p>
+            <Button
+              variant="outline"
+              className="rounded-lg mt-1"
+              onClick={onLeaveGame}
+            >
+              Exit Game
+              <ChevronRight size={20} className="ml-0.5 w-4 h-4" />
+            </Button>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
