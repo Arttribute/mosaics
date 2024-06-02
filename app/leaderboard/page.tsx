54d8671d3f4366c619d0b9a72eb7e3a4f8b9669e
adala-wanyande <benardwanyande@gmail.com>
2024-06-02 14:24:28 +0300

    fix: add suspense fallback to leaderboard page

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,12 +1,15 @@
 import AppBar from "@/components/layout/AppBar";
-import SinglePlayerLeaderboard from "@/components/leaderboard/SinglePlayerLeaderboard";
+import { Suspense } from "react";
+import Leaderboard from "@/components/leaderboard/Leaderboard";
 
-export default function SinglePlayerLeaderboardPage() {
+export default function LeaderboardPage() {
   return (
     <>
       <AppBar />
       <div className="container mx-auto p-4 mt-16">
-        <SinglePlayerLeaderboard />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Leaderboard />
+        </Suspense>
       </div>
     </>
   );
