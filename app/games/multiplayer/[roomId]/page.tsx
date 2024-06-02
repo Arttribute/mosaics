4ed65c6e0c6dd0c45b2ed571e9019cc482d7cf95
bashybaranaba <bashybaranaba@gmail.com>
2024-06-02 13:57:37 +0300

    feat: add navbar to game pages

--- app/games/multiplayer/[roomId]/page.tsx
+++ app/games/multiplayer/[roomId]/page.tsx
@@ -2,14 +2,19 @@
 import { AblyProvider, ChannelProvider } from "ably/react";
 import * as Ably from "ably";
 import Multiplayer from "@/components/game/multiplayer";
+import AppBar from "@/components/layout/AppBar";
+
 export default function Game({ params }: { params: { roomId: string } }) {
   const client = new Ably.Realtime({ authUrl: "/api" });
 
   return (
-    <AblyProvider client={client}>
-      <ChannelProvider channelName={params.roomId}>
-        <Multiplayer roomId={params.roomId} />
-      </ChannelProvider>
-    </AblyProvider>
+    <>
+      <AppBar />{" "}
+      <AblyProvider client={client}>
+        <ChannelProvider channelName={params.roomId}>
+          <Multiplayer roomId={params.roomId} />
+        </ChannelProvider>
+      </AblyProvider>
+    </>
   );
 }
