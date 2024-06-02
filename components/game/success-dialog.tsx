b3d2855494badfc537a56ad1e71f9ddfa8fc5f05
bashybaranaba <bashybaranaba@gmail.com>
2024-06-02 03:27:22 +0300

    feat: add claim reward and mint nft button

--- components/game/success-dialog.tsx
+++ components/game/success-dialog.tsx
@@ -5,43 +5,61 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { ChevronRight } from "lucide-react";
 
-import {
-  Dialog,
-  DialogContent,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
 
 export default function SuccessDialog({
   open,
   solution,
+  mode,
+  stakeAmount,
+  multiplier,
   onContinue,
+  onLeaveGame,
 }: {
   open: boolean;
   solution: string;
+  mode: "single" | "multiplayer" | "earn";
+  stakeAmount?: string;
+  multiplier?: number;
   onContinue: () => void;
+  onLeaveGame: () => void;
 }) {
   return (
     <Dialog open={open}>
-      <DialogContent>
+      <DialogContent className="w-96">
         <div className="w-full">
           <div className="p-2 flex flex-col items-center justify-center">
             <Image
               src={solution}
-              width={500}
-              height={500}
+              width={400}
+              height={400}
               alt={"game"}
               className="aspect-square rounded-md m-1"
             />
             <p className="text-xl font-semibold  m-2">Puzzle solved 🎉</p>
-            <Button
-              variant="outline"
-              className="rounded-lg mt-1"
-              onClick={onContinue}
-            >
-              Go to next puzzle
-              <ChevronRight size={20} className="ml-0.5 w-4 h-4" />
-            </Button>
+            {mode === "earn" && multiplier && (
+              <p className="text-sm  m-2">
+                You earned {Number(stakeAmount) * multiplier} FIL
+              </p>
+            )}
+            <div className="flex flex-col items-center justify-center w-full">
+              <Button
+                variant="outline"
+                className="rounded-lg  mt-1 w-full border-gray-400"
+                onClick={onContinue}
+              >
+                Go to next puzzle
+                <ChevronRight size={20} className="ml-0.5 w-4 h-4" />
+              </Button>
+              <Button
+                className="rounded-lg mt-1 border-gray-500 w-full"
+                onClick={onLeaveGame}
+              >
+                {mode === "earn"
+                  ? "Claim reward and NFT"
+                  : "Claim NFT and End game"}
+              </Button>
+            </div>
           </div>
         </div>
       </DialogContent>
